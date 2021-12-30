97bdf3eabe77be6d930f1f0d0db5db3966428b6f
equaliser0 <accounts@equaliser.minimalcity.de>
2021-12-30 13:31:42 +0100

    add types temp

---
+++ contracts/types/CunoroAccessControlled.sol
@@ -0,0 +1,55 @@
+// SPDX-License-Identifier: AGPL-3.0-only
+pragma solidity >=0.7.5;
+
+import "../interfaces/ICunoroAuthority.sol";
+
+abstract contract CunoroAccessControlled {
+
+    /* ========== EVENTS ========== */
+
+    event AuthorityUpdated(ICunoroAuthority indexed authority);
+
+    string UNAUTHORIZED = "UNAUTHORIZED"; // save gas
+
+    /* ========== STATE VARIABLES ========== */
+
+    ICunoroAuthority public authority;
+
+
+    /* ========== Constructor ========== */
+
+    constructor(ICunoroAuthority _authority) {
+        authority = _authority;
+        emit AuthorityUpdated(_authority);
+    }
+    
+
+    /* ========== MODIFIERS ========== */
+    
+    modifier onlyGovernor() {
+        require(msg.sender == authority.governor(), UNAUTHORIZED);
+        _;
+    }
+    
+    modifier onlyGuardian() {
+        require(msg.sender == authority.guardian(), UNAUTHORIZED);
+        _;
+    }
+    
+    modifier onlyPolicy() {
+        require(msg.sender == authority.policy(), UNAUTHORIZED);
+        _;
+    }
+
+    modifier onlyVault() {
+        require(msg.sender == authority.vault(), UNAUTHORIZED);
+        _;
+    }
+    
+    /* ========== GOV ONLY ========== */
+    
+    function setAuthority(ICunoroAuthority _newAuthority) external onlyGovernor {
+        authority = _newAuthority;
+        emit AuthorityUpdated(_newAuthority);
+    }
+}
